b2a5c2cbf008d39330c7fd769546039f6cda993f
liulijun <1937249556@qq.com>
2020-04-07 17:55:30 +0800

    提交：Calendar.vue

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,13 +2,13 @@
 <div class="calendar-wrap">
     <div class="year-month">
         <div class="left">
-            <span class="caret" number="year-dec"></span>  <!-- 年份递减图标 -->
-            <span class="caret" number="month-dec"></span>  <!-- 月份递减图标 -->
+            <span class="caret" number="year-dec" @click="calculate(calendar.year-1,calendar.month)"></span>  <!-- 年份递减图标 -->
+            <span class="caret" number="month-dec" @click="calculate(calendar.year,calendar.month-1)"></span>  <!-- 月份递减图标 -->
         </div>         
-        <div class="middle"><i>{{calendr.year}}年{{calendr.month+1}}月</i></div>
+        <div class="middle"><i>{{calendar.year}}年{{calendar.month+1}}月</i></div>
         <div class="right">
-            <span class="caret" number="month-inc"></span>    <!-- 月份递增图标 -->   
-            <span class="caret" number="year-inc" @click="yearGrow()"></span>   <!-- 年份递增图标 -->   
+            <span class="caret" number="month-inc" @click="calculate(calendar.year,calendar.month+1)"></span>    <!-- 月份递增图标 -->   
+            <span class="caret" number="year-inc" @click="calculate(calendar.year+1,calendar.month)"></span>   <!-- 年份递增图标 -->   
         </div>            
     </div>
     <div class="week-wrap">
@@ -20,8 +20,8 @@
         <ul class="days">
             <!-- 如果第1天不是星期日，则增加相应的空白日期占据位置，以便于日期与星期对齐 。例如，星期一增加1个空白日期用于占位-->
             <template v-for="date of daylist">
-                <li v-if="date.number==calendr.day" day :valid="date.valid">{{date.number}}</li>
-                <li v-else :valid="date.valid">{{date.number}}</li>
+                <li v-if="date.number==calendar.day" day :valid="date.valid" :current="calendar.isCurrent">{{date.number}}</li>
+                <li v-else :valid="date.valid" :current="calendar.isCurrent">{{date.number}}</li>
             </template>
             
         </ul>
@@ -33,6 +33,7 @@
 
 <script type="text/javascript">
 
+
 export default{
     name:'calendar',
     props:{        
@@ -42,12 +43,13 @@ export default{
     data:function(){
       return { 
         week:['日','一','二','三','四','五','六'], 
-        calendr:{   //日历数据列表，通过calculate()函数计算得出。
+        calendar:{   //日历数据列表，通过calculate()函数计算得出。
             year:0,        //年份字符串
             month:0,       //月份字符串
             day:0,          //当日
-            days:31,        //月份天数
+            days:0,        //月份天数
             weekday:0,      //月份第一天星期几。星期天：0；星期一：1；星期二：2；星期三：3；星期四：4；星期五：5；星期六：6。            
+            isCurrent:true,    //是否现在日期（即没有对年月进行翻页）
             isLeapyear:false,   //是否闰年
             beforeyesterday:'', //前天
             yesterday:'',   //昨天
@@ -55,86 +57,86 @@ export default{
             tomorrow:'',    //明天
             aftertomorrow:'',  //后天            
         },
-        daylist:[
-            {number:'1',valid:true},
-            {number:'2',valid:true},
-            {number:'3',valid:true},
-            {number:'4',valid:true},
-            {number:'5',valid:true},
-            {number:'6',valid:true},
-            {number:'7',valid:true},
-            {number:'8',valid:true},
-            {number:'9',valid:true},
-            {number:'10',valid:true},
-            {number:'11',valid:true},
-            {number:'12',valid:true},
-            {number:'13',valid:true},
-            {number:'14',valid:true},
-            {number:'15',valid:true},
-            {number:'16',valid:true},
-            {number:'17',valid:true},
-            {number:'18',valid:true},
-            {number:'19',valid:true},
-            {number:'20',valid:true},
-            {number:'21',valid:true},
-            {number:'22',valid:true},
-            {number:'23',valid:true},
-            {number:'24',valid:true},
-            {number:'25',valid:true},
-            {number:'26',valid:true},
-            {number:'27',valid:true},
-            {number:'28',valid:true},
-            {number:'29',valid:true},
-            {number:'30',valid:true},
-            {number:'31',valid:true},
-        ],
+        daylist:[],        
       };      
     },
     computed:{
-        calculate(){             
-        }
 
     },
     methods:{
-        yearGrow(){ //递增年份
-            
+        calculate(year,month){ //日历计算函数
+            //let year=this.calendar.year+1;
+            //let month=this.calendar.month;
+            if(month>=12 || month<0){
+                return;
+            }
+            let days = new Date(year, month + 1, 0).getDate();  //计算递增/递减后的当前月份天数
+            let firstDay = new Date(year, month, 1); //递增/递减后年份后的当前月的第一天的日期
+            let weekday = firstDay.getDay();        //递增/递减后年份后的当前月份第一天是星期几
+            let count=weekday-this.calendar.weekday;  //递增/递减前后，对月份第一天的星期几的数字进行相减，如果是正数则添加空白占位，负数则减少空白占位，以便日期与星期正确对齐。
+            if(count>0){ //正数则添加空白占位，以便日期与星期正确对齐。
+                for(var i = 0,len=count; i < len; i++) {                
+                    this.daylist.unshift({number:'',valid:false});
+                }
+            }
+            if(count<0){ //负数则减少空白占位，以便日期与星期正确对齐。
+                for(var i = 0,len=Math.abs(count); i < len; i++) {                
+                    this.daylist.shift();
+                }
+            }
+            count=days-this.calendar.days;  //将递增/递减前后的当前月份总天数相减。如果是正数则添加日期格，负数则减少日期格。以便日期的完整性与星期正确对齐。
+            if(count>0){ //正数则在尾部添加日期格,以便日期的完整性与星期正确对齐。？？？？
+                for(var i=this.calendar.days,len=days;i<len;i++){
+                    this.daylist.push({number:i+1,valid:true});
+                }
+            }
+            if(count<0){ //负数则在尾部减少日期格，以便日期的完整性与星期正确对齐。
+                for(var i = 0,len=Math.abs(count); i < len; i++) {                
+                    this.daylist.pop();
+                }
+            }
+            //保存递增/递减后的数据
+            this.calendar.year=year;
+            this.calendar.month=month;
+            this.calendar.days=days;
+            this.calendar.firstDay=firstDay;
+            this.calendar.weekday =weekday;
+            this.calendar.isCurrent=false;
         },
-        yearReduce(){ //递减年份
-
-        },
-        monthGrow(){ //递增月份
-
-        },
-        monthReduce(){ //递减月份
-
-        }
-        
+       
     },
     watch:{},
     components:{},
     // 生命周期钩子
     beforeCreate:function(){},
     created:function(){ 
-        //日历计算：根据当天日期计算，将计算结果存储到calendr对象中。
+        //日历计算：根据当天日期计算，将计算结果存储到calendar对象中。
         var date = new Date();   //获取当天日期
-        this.calendr.year=date.getFullYear();   //计算当前年份
-        this.calendr.month=date.getMonth();  //计算当前月份，注意这里的月是从0开始计数的。
-        this.calendr.day=date.getDate();     //计算当前日
-        this.calendr.days = new Date(this.calendr.year, this.calendr.month + 1, 0).getDate();  //计算当前月份天数
-        var firstDay = new Date(this.calendr.year, this.calendr.month, 1); //当前月的第一天的日期
-        this.calendr.weekday = firstDay.getDay(); //当前第一天是星期几
-        if(this.calendr.weekday>0){  //如果第1天不是星期日，则增加相应的空白日期占据位置，以便于日期与星期对齐 。例如，星期一增加1个空白日期用于占位
-            for(var i = 0,len=this.calendr.weekday; i < len; i++) {                
+        this.calendar.year=date.getFullYear();   //计算当前年份
+        this.calendar.month=date.getMonth();  //计算当前月份，注意这里的月是从0开始计数的。
+        this.calendar.day=date.getDate();     //计算当前日
+        this.calendar.days = new Date(this.calendar.year, this.calendar.month + 1, 0).getDate();  //计算当前月份天数
+        var firstDay = new Date(this.calendar.year, this.calendar.month, 1); //当前月的第一天的日期
+        this.calendar.weekday = firstDay.getDay(); //当前第一天是星期几
+        //创建标准月份天数（31天）列表
+        for (var i =1; i<=31; i++) {
+            this.daylist.push({number:i,valid:true});
+        }
+        console.log(`The copyObj is ${JSON.stringify(this.daylist)}`);  
+        //如果第1天不是星期日，则增加相应的空白日期占据位置，以便于日期与星期对齐 。例如，星期一增加1个空白日期用于占位      
+        if(this.calendar.weekday>0){  
+            for(var i = 0,len=this.calendar.weekday; i < len; i++) {                
                 this.daylist.unshift({number:'',valid:false});
             }
         }
-        if(this.calendr.days<31){
-            for(var i = 0,len=31-this.calendr.days; i < len; i++) {                
+        //如果当前月份天数小于标准月份（31天），则从标准月份数组daylist中删除。
+        if(this.calendar.days<31){
+            for(var i = 0,len=31-this.calendar.days; i < len; i++) {                
                 this.daylist.pop();
             }
             
         }
-        console.log(this.daylist);
+        console.log(`当前月份天数： ${JSON.stringify(this.daylist)}`); 
 
     },
     beforeMount:function(){},
